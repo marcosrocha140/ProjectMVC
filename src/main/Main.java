a8cad65e8e7cb96a3d5bda9cbf6eae179685c25c
Marcos Rocha <rmarcso140@gmail.com>
2024-11-13 22:18:43 -0300

    alteraçao no arquivo main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 import daoimplementation.UsuarioDAOImpl;
 import daointerfaces.UsuarioDAO;
 import model.Usuario;
@@ -8,30 +10,54 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Adicionar usuario
+		int value = 0;
+		Scanner sc = new Scanner(System.in);
 		UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
-		//Usuario novoUsuario = new Usuario("Marcelo", "marcelc@gmail.com");
-		//usuarioDAO.adicionarUsuario(novoUsuario);
 		
 		
+		
+		while(value == 0) {
+			
+			System.out.println("1 - Cadastrar usuario."+"\n"+"2 - Buscar Usuario."+"3 - Listar usuarios."+"\n"+"4 - Atualiazar Usuario."+"\n"+"5 - Excluir usuario."
+					+"\n"+"0 - Sair");
+					
+			value = sc.nextInt();
+			switch(value) {
+				case 1:
+					Usuario novoUsuario = new Usuario("Marcos", "marcos@gmail.com");
+					usuarioDAO.adicionarUsuario(novoUsuario);
+					break;
+				case 2:
+					Usuario usuario = usuarioDAO.buscarUsuario(1);
+					System.out.println("Usuario encontrado: "+ "ID: "+usuario.getId()+"Nome: "+usuario.getNome() + usuario.getEmail());
+					break;
+				case 3:
+					System.out.println("Listas de usuarios: ");
+					for(Usuario u : usuarioDAO.listarUsuarios()){
+						System.out.println("ID: " + u.getId() + "Nome: " + u.getNome() + "E-mail: " + u.getEmail());
+					}
+					break;
+				case 4:
+					/*Usuario usuario = new Usuario();
+					usuario.setId(2);
+					usuario.setNome("Marcos Rocha");
+					usuario.setEmail("rmarcos140@gmail.com");
+					usuarioDAO.atualizarUsuario(usuario);*/
+					break;
+				case 5:
+					usuarioDAO.deletarUsuario(1);
+					break;
+				case 0:
+					System.out.println("Você saiu!");
+					break;
+				default:
+					System.out.println("Opção Invalida!!!");
+			}
+		}
+		//Adicionar usuario			
 		//Buscar Usuario
-		//Usuario usuario = usuarioDAO.buscarUsuario(1);
-		//System.out.println("Usuario encontrado: "+ "ID: "+usuario.getId()+"Nome: "+usuario.getNome() + usuario.getEmail());
-	
-	
-		/*System.out.println("Listas de usuarios: ");
-		for(Usuario u : usuarioDAO.listarUsuarios()){
-			System.out.println("ID: " + u.getId() + "Nome: " + u.getNome() + "E-mail: " + u.getEmail());
-		}*/
-	
 		//Atualizar usuario
-		/*Usuario usuario = new Usuario();
-		usuario.setId(2);
-		usuario.setNome("Marcos Rocha");
-		usuario.setEmail("rmarcos140@gmail.com");
-		usuarioDAO.atualizarUsuario(usuario);*/
-		
 		//Deletar usuario
-		//usuarioDAO.deletarUsuario(1);
+		
 	}
 }
